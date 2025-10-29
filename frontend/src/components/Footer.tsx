104b63e9f543818b029204093538fb739c2075cd
yoyobazzi3 <yoyobazzi3@gmail.com>
2025-10-28 18:04:45 -0700

    yb-created react app with TypeScript

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ textAlign: "center", marginTop: "2rem", padding: "1rem" }}>
+      <small>© {new Date().getFullYear()} My App</small>
+    </footer>
+  );
+}
+
+export default Footer;
